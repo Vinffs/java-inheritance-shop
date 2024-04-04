66dfb9795a57195faf1ca551356ba1a1122e993b
Vinffs <vin.demiranda@gmail.com>
2024-04-04 11:08:51 +0200

    created conditions to verify which product, created product + store in array, print to console cart

--- src/org/learning/shop/Cart.java
+++ src/org/learning/shop/Cart.java
@@ -1,6 +1,8 @@
 package org.learning.shop;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Cart {
@@ -40,11 +42,75 @@ public class Cart {
             } catch(NumberFormatException e) {
                 System.out.println("Invalid choice, please select a product by index.");
                 i--;
+            } finally {
+                System.out.print("Name: ");
+                String name = scan.nextLine();
+
+                System.out.print("Brand: ");
+                String brand = scan.nextLine();
+
+                System.out.print("Price: ");
+                double price = Double.parseDouble(scan.nextLine().replaceAll(",", "."));
+
+
+                switch (product) {
+                    case 1:
+                        System.out.print("imei: ");
+                        String imei = scan.nextLine();
+
+                        System.out.print("Storage (GB): ");
+                        int storage = Integer.parseInt(scan.nextLine());
+
+                        Smarthphone smarthphone = new Smarthphone(name, brand, price, imei, storage);
+                        cart[i] = smarthphone;
+                        break;
+
+                    case 2:
+                        System.out.print("inches: ");
+                        int inches = Integer.parseInt(scan.nextLine());
+
+                        System.out.print("Is it Smart? Y/N ");
+                        boolean isSmart = Objects.equals(scan.nextLine(), "Y");
+
+                        Television television = new Television(name, brand, price, inches, isSmart);
+                        cart[i] = television;
+                        break;
+
+                    case 3:
+                        System.out.print("color: ");
+                        String color = scan.nextLine();
+
+                        System.out.print("Is it Wireless? Y/N ");
+                        boolean isWireless = Objects.equals(scan.nextLine(), "Y");
+
+                        Headphone headphone = new Headphone(name, brand, price, color, isWireless);
+
+                        cart[i] = headphone;
+                        break;
+
+                }
             }
 
         }
 
+        BigDecimal totalCartSum = BigDecimal.ZERO;
+        for (int i = 0; i < cart.length; i++) {
+            System.out.println("Product " + (i+1) + ":\n" + cart[i].toString());
+            System.out.println();
+            System.out.println("---------------------------------------");
+            System.out.println();
+            BigDecimal price = cart[i].getGrossPrice();
+            totalCartSum = totalCartSum.add(price);
+        }
 
+        System.out.println("Total Cart (VAT included) : " + totalCartSum + " €");
+
+
+
+
+
+
+        scan.close();
     }
 }
 
