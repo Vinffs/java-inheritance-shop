161f59c72ab5bb785e2539b89f7d9581818cd7f5
Vinffs <vin.demiranda@gmail.com>
2024-04-04 10:21:30 +0200

    ask user for array length and try catch input error

--- src/org/learning/shop/Cart.java
+++ src/org/learning/shop/Cart.java
@@ -1,7 +1,27 @@
 package org.learning.shop;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Cart {
     public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    boolean flag = false;
+    int size = 0;
+    Product[] cart = null;
+    while(!flag) {
+        try {
+            System.out.print("Please tell us how many products would you like to add to cart: ");
+            size = Integer.parseInt(scan.nextLine());
+            flag = true;
+        } catch (NumberFormatException e) {
+            System.out.println("Error " + e.getMessage());
+            System.out.println("invalid input, please insert a number!");
+        } finally {
+            cart = new Product[size];
+        }
+    }
 
 
     }
