f23dcb81990e16ac9cf48cb2b3e98e7ae2a8368f
Vinffs <vin.demiranda@gmail.com>
2024-04-04 09:47:19 +0200

    created methods, getters and setters for Product

---
+++ src/org/learning/shop/Product.java
@@ -0,0 +1,81 @@
+package org.learning.shop;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Random;
+
+public class Product {
+
+    // ATTRIBUTES
+    private int code;
+    private String name, brand;
+    private BigDecimal price;
+    private final double vat = 0.23;
+
+    // CONSTRUCTORS
+
+    public Product(String name, String brand, double price) {
+        this.code = generateCode();
+        this.name = name;
+        this.brand = brand;
+        this.price = correctPrice(price);
+    }
+
+
+    // METHODS
+
+    private int generateCode() {
+        Random randomGenerator = new Random();
+        return randomGenerator.nextInt(99_999_999);
+    }
+
+    private BigDecimal correctPrice(double price) {
+        return new BigDecimal(price).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    // GETTERS
+
+    public BigDecimal getPrice() {
+        return this.price;
+    }
+
+    public BigDecimal getGrossPrice() {
+        BigDecimal vat = new BigDecimal(this.vat);
+        BigDecimal gross = this.price.add(this.price.multiply(vat));
+
+        return gross.setScale(2, RoundingMode.HALF_UP);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "code=" + code + ", name='" + name + '\'' + ", brand='" + brand + '\'' + ", price=" + price + ", vat=" + (vat * 100) + "%" + '}';
+    }
+
+    public int getCode() {
+        return this.code;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+
+
+    // SETTERS
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
